d7cafd9f541d7b8803eddb736fbc756da9cf0212
FranciscoEsquer <franciscod.esquer@potros.itson.edu.mx>
2023-02-08 21:15:09 -0700

    commit inicial

---
+++ app/src/main/java/esquer/francisco/thecheezery/MenuActivity.kt
@@ -0,0 +1,11 @@
+package esquer.francisco.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
